e558dbf2949bf4666f9bd1ad319a808f254c51e0
test <dtdsf@gfgffgd.com>
2020-08-29 18:13:28 +0900

    20200829

---
+++ Vuejs/vue04Webpack/webpack-test/src/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="jumbotron text-center" style="margin-bottom:0">
+    <h1>Footer.vue</h1>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ Vuejs/vue04Webpack/webpack-test/src/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="jumbotron text-center" style="margin-bottom:0">
+    <h1>Header.vue</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
